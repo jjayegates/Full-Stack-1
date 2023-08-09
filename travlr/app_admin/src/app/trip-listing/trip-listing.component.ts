8c8f87eb76f945c8f5fce70ed65a2fd8ae146483
Janece Gates <Janece.Gates@snhu.edu>
2023-08-09 11:56:36 -0500

    edit button not working

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'; 
+import {Router } from "@angular/router";
 // import { trips } from '../data/trips'; 
 import { TripDataService } from '../services/trip-data.service'; 
 import { Trip } from '../models/trip'; 
@@ -14,7 +15,14 @@ export class TripListingComponent implements OnInit {
   trips: Trip[]; 
   message: string; 
   
-  constructor(private tripDataService: TripDataService) { } 
+  constructor(
+    private tripDataService: TripDataService, 
+    private router: Router
+    ) { } 
+  
+    private addTrip(): void {
+      this.router.navigate(['add-trip']);
+    }
   
   private getTrips(): void { 
     console.log('Inside TripListingComponent#getTrips');

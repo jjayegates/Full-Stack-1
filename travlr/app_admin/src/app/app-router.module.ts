3793c15a12d9da5cf0088796bdc14c4d5658f361
Janece Gates <Janece.Gates@snhu.edu>
2023-08-13 12:50:02 -0500

    Everything working

--- travlr/app_admin/src/app/app-router.module.ts
+++ travlr/app_admin/src/app/app-router.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' }
 ]
 
-@NgModule({ 
+@NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
